92f81844efc0c6215cb98390cbd87238e134d1cd
Luiz Otavio <luizomf@gmail.com>
2020-07-09 08:41:32 -0300

    Configurando a logica

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App(): JSX.Element {
   return (
     <div className="container">
       <PomodoroTimer
-        pomodoroTime={10}
-        shortRestTime={2}
-        longRestTime={5}
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
         cycles={4}
       />
     </div>
