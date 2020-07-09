bba56df50c85e45034a1638d2560e8a39c71614a
Luiz Otavio <luizomf@gmail.com>
2020-07-09 12:27:11 -0300

    corrigindo timing

--- src/utils/zero-left.ts
+++ src/utils/zero-left.ts
@@ -1,2 +1,3 @@
-const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
+const zeroLeft = (n: number): string =>
+  Math.floor(n).toString().padStart(2, '0');
 export { zeroLeft };
