4a3d19cf3ca90b36494973c9f987f456bc0c161b
Luiz Otavio <luizomf@gmail.com>
2020-07-10 02:24:48 -0300

    Comentarios sobre tempo

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App(): JSX.Element {
   return (
     <div className="container">
       <PomodoroTimer
-        pomodoroTime={1500}
-        shortRestTime={300}
-        longRestTime={900}
+        pomodoroTime={1500} // 25min
+        shortRestTime={300} // 5min
+        longRestTime={900} // 15min
         cycles={4}
       />
     </div>
